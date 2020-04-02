5fdcc402793f197618f5723272520829a7745076
BBFISH <44829092+xmliszt@users.noreply.github.com>
2020-04-03 00:12:11 +0800

    cool stuff! perspective change on hover

--- js/index.js
+++ js/index.js
@@ -1,7 +1,86 @@
 const ISTD = "Information Systems Technology and Design";
 const ESD = "Engineering Systems and Design";
+var container = document.querySelector(".container-x");
+var inner = document.querySelector(".box");
 
 $(document).ready(()=>{
+
+    $(window).hover((e) =>{
+        // console.log(inner);
+        var x = e.clientX, y = e.clientY,
+        elementMouseIsOver = document.elementFromPoint(x, y);
+        if (elementMouseIsOver != null){
+            if (elementMouseIsOver.className == "container-x"){
+                container = elementMouseIsOver;
+                inner = elementMouseIsOver.childNodes[1];    
+                // Mouse 
+                var mouse = {
+                    _x: 0,
+                    _y: 0,
+                    x: 0,
+                    y: 0,
+                    updatePosition: function(event) {
+                        var e = event || window.event;
+                        this.x = e.clientX - this._x;
+                        this.y = (e.clientY - this._y) * -1;
+                    },
+                    setOrigin: function(e) {
+                        if (e != null){
+                            this._x = e.offsetLeft + Math.floor(e.offsetWidth/2);
+                            this._y = e.offsetTop + Math.floor(e.offsetHeight/2);
+                        }
+                    },
+                    show: function() { return '(' + this.x + ', ' + this.y + ')'; }
+                }
+                // Track the mouse position relative to the center of the container.
+                mouse.setOrigin(container);
+            
+                var counter = 0;
+                var updateRate = 10;
+                var isTimeToUpdate = function() {
+                    return counter++ % updateRate === 0;
+                };
+            
+                var update = function(event) {
+                    mouse.updatePosition(event);
+                    updateTransformStyle(
+                    (mouse.y / inner.offsetHeight/2).toFixed(2),
+                    (mouse.x / inner.offsetWidth/2).toFixed(2)
+                    );
+                };
+                
+                var updateTransformStyle = function(x, y) {
+                    if (inner != null){
+                        var style = "rotateX(" + 3*x + "deg) rotateY(" + 3*y + "deg)";
+                        inner.style.transform = style;
+                        inner.style.webkitTransform = style;
+                        inner.style.mozTransform = style;
+                        inner.style.msTransform = style;
+                        inner.style.oTransform = style;
+                    }
+                };
+            
+                var onMouseEnterHandler = function(event) {
+                    update(event);
+                };
+                var onMouseLeaveHandler = function() {
+                    inner.style = "";
+                };
+                var onMouseMoveHandler = function(event) {
+                    if (isTimeToUpdate()) {
+                        update(event);
+                    }
+                };
+                container.onmouseenter = onMouseEnterHandler;
+                container.onmouseleave = onMouseLeaveHandler;
+                container.onmousemove = onMouseMoveHandler;
+            } 
+        }
+        
+    });
+    
+    
+    
     $('#lexuan').click(()=>{
         putInfo("Soong Le Xuan", ISTD, "Prototyping, Strategic Thinking, Blockchain, Problem-Solving","Java, JavaScript, CSS, HTML, C, Python, Lucid", "https://www.linkedin.com/in/le-xuan-soong-81687818b/","https://github.com/hihithisisme")
         $('#popinfo').fadeIn();
@@ -28,7 +107,7 @@ $(document).ready(()=>{
     });
 
     $('#jeslyn').click(()=>{
-        putInfo("Peh Wei Ting, Jeslyn", ISTD, "Crochet, Baking, Stalking","Java, Python, Scripting in Adobe Illustrator (Javascript), abit of C", "https://www.linkedin.com/in/jeslynpwt/","https://github.com/jesterlyn")
+        putInfo("Peh Wei Ting, Jeslyn", ISTD, "Crochet, Baking, Personal information reconnaissance","Java, Python, Scripting in Adobe Illustrator (Javascript), abit of C", "https://www.linkedin.com/in/jeslynpwt/","https://github.com/jesterlyn")
         $('#popinfo').fadeIn();
     });
 
